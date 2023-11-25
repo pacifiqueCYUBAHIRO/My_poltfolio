9ce86e73c549ff08a7c22a57fe4ff9262a821303
pacifiqueCYUBAHIRO <118057508+pacifiqueCYUBAHIRO@users.noreply.github.com>
2023-11-25 20:38:50 +0200

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import '@fortawesome/fontawesome-free/css/all.css';
-import './Navbar.css'; 
+import { Link, animateScroll as scroll, scroller } from 'react-scroll';
+import './NavBar.css';
 import logo from './images/logo.png';
 
 const Navbar = () => {
@@ -12,17 +13,79 @@ const Navbar = () => {
     setIsOpen(!isOpen);
   };
 
+  const scrollToSection = (sectionId) => {
+    scroller.scrollTo(sectionId, {
+      duration: 800,
+      delay: 0,
+      smooth: 'easeInOutQuart',
+    });
+    setIsOpen(false); // Close the navbar after clicking a link
+  };
+
   return (
     <nav className={`navbar ${isOpen ? 'open' : ''}`}>
       <div className="logo">
         <img src={logo} alt="" />
       </div>
       <div className={`nav-links ${isOpen ? 'open' : ''}`}>
-        <li>Home</li>
-        <li>About</li>
-        <li>Blogs</li>
-        <li>Poltfolio</li> 
-        <li>Contact</li>
+
+      
+  <li>
+    <Link
+      to="home"
+      spy={true}
+      smooth={true}
+      offset={-70}
+      duration={500}
+    >
+      Home
+    </Link>
+  </li>
+  <li>
+    <Link
+      to="about"
+      spy={true}
+      smooth={true}
+      offset={-70}
+      duration={500}
+    >
+      About
+    </Link>
+  </li>
+  <li>
+    <Link
+      to="blogs"
+      spy={true}
+      smooth={true}
+      offset={-70}
+      duration={500}
+    >
+      Blogs
+    </Link>
+  </li>
+  <li>
+    <Link
+      to="poltfolio"
+      spy={true}
+      smooth={true}
+      offset={-70}
+      duration={500}
+    >
+      Poltfolio
+    </Link>
+  </li>
+  <li>
+    <Link
+      to="contacts"
+      spy={true}
+      smooth={true}
+      offset={-70}
+      duration={500}
+    >
+      Contacts
+    </Link>
+  </li>
+
         <button>Sign In</button>
       </div>
 
